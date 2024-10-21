24c776e072ae5d313200e1fc7f9d8c61a7c3b4cb
tara1382 <taragasemi00@gmail.com>
2024-10-21 13:52:08 -0700

    Add files via upload

---
+++ 1.ino
@@ -0,0 +1,22 @@
+int pin = 8;
+int ledpin = 2;
+int buttonstate = 0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledpin, OUTPUT);
+pinMode(buttonstate, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+buttonstate = digitalRead(pin);
+if (buttonstate == HIGH)
+{
+  digitalWrite(ledpin , HIGH);
+}
+else 
+{
+  digitalWrite(ledpin , LOW);  
+}
+  }
